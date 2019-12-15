b5e3cd16a6640993a79862f52fdcaa9f962fb078
Daniel Samet <danielsamet98@gmail.com>
2019-12-15 23:45:29 +0000

    updated queries.sql with MODERATE queries;
    Signed-off-by: Daniel Samet <danielsamet98@gmail.com>

--- queries.sql
+++ queries.sql
@@ -2,11 +2,13 @@
 SELECT * FROM Property WHERE sale_price < 2000 AND size_unit = 5;
 
 /* BASIC: Select Properties with sale_price < 2000 AND address 10 Sinclair Grove */
-SELECT * FROM Property INNER JOIN Address A2 on Property.address_id = A2.AddressID WHERE sale_price < 2000 AND address_line_1 = "10 Sinclair Grove";
+SELECT * FROM Property INNER JOIN Address A2 on Property.address_id = A2.AddressID WHERE sale_price < 2000 AND address_line_1 = '10 Sinclair Grove';
 
 /* MODERATE: Select Houses with 2 rooms */
+SELECT * FROM House INNER JOIN Property P on House.HouseID = P.PropertyID WHERE (SELECT COUNT(*) FROM Room WHERE RoomID = P.PropertyID) = 2;
 
 /* MODERATE: Select Flats with >= 2 rooms AND leasehold_frequency < 5 */
+SELECT * FROM Flat INNER JOIN Property P on Flat.FlatID = P.PropertyID WHERE (SELECT COUNT(*) FROM Room WHERE RoomID = P.PropertyID) >= 2 AND leasehold_frequency < 5;
 
 /* ADVANCED: Select (number of flats) with more than 50 clicks since 10 December */
 
