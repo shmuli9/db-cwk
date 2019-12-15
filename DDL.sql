bacd74f30dc847744552603cfae246c134d0a187
Daniel Samet <danielsamet98@gmail.com>
2019-12-15 23:37:01 +0000

    added the DDL script;
    Signed-off-by: Daniel Samet <danielsamet98@gmail.com>

---
+++ DDL.sql
@@ -0,0 +1,52 @@
+CREATE TABLE Address (
+    AddressID int Primary Key AUTO_INCREMENT,
+    address_line_1 varchar(255),
+    address_line_2 varchar(255),
+    post_code varchar(10)
+);
+
+CREATE TABLE Property (
+    PropertyID int Primary Key AUTO_INCREMENT,
+    sale_price int,
+    address_id int,
+    FOREIGN KEY (address_id) REFERENCES Address(AddressID),
+    size_unit varchar(255)
+);
+
+CREATE TABLE House (
+    HouseID int Primary Key AUTO_INCREMENT,
+    FOREIGN KEY (HouseID) REFERENCES Property(PropertyID)
+);
+
+CREATE TABLE Flat (
+    FlatID int Primary Key AUTO_INCREMENT,
+    FOREIGN KEY (FlatID) REFERENCES Property(PropertyID),
+    leasehold_payment int,
+    leasehold_frequency int
+);
+
+CREATE TABLE Room (
+    RoomID int Primary Key AUTO_INCREMENT,
+    size int,
+    property_id int,
+    FOREIGN KEY (property_id) REFERENCES Property(PropertyID),
+    type varchar(32)
+);
+
+CREATE TABLE User (
+    UserID int Primary Key AUTO_INCREMENT,
+    address_id int,
+    FOREIGN KEY (address_id) REFERENCES Address(AddressID),
+    age int,
+    f_name varchar(32),
+    l_name varchar(32)
+);
+
+CREATE TABLE Click (
+    ClickID int Primary Key AUTO_INCREMENT,
+    dtime datetime,
+    property_id int,
+    FOREIGN KEY (property_id) REFERENCES Property(PropertyID),
+    UserID int,
+    FOREIGN KEY (UserID) REFERENCES User(UserID)
+);
