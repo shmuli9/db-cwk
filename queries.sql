2f906d90e90c58613ba93f32e5172557ba7988fd
Daniel Samet <danielsamet98@gmail.com>
2019-12-16 00:11:02 +0000

    updated queries.sql;
    Signed-off-by: Daniel Samet <danielsamet98@gmail.com>

--- queries.sql
+++ queries.sql
@@ -1,15 +1,17 @@
-/* BASIC: Select Properties with sale_price < 2000 AND size_unit = 5 */
-SELECT * FROM Property WHERE sale_price < 2000 AND size_unit = 5;
+/* BASIC: Select Properties with sale_price AND size_unit */
+SELECT * FROM Property WHERE sale_price < 500000 AND size_unit = 'metres';
 
-/* BASIC: Select Properties with sale_price < 2000 AND address 10 Sinclair Grove */
-SELECT * FROM Property INNER JOIN Address A2 on Property.address_id = A2.AddressID WHERE sale_price < 2000 AND address_line_1 = '10 Sinclair Grove';
+/* BASIC: Select Properties with sale_price AND address*/
+SELECT * FROM Property INNER JOIN Address A2 on Property.address_id = A2.AddressID WHERE sale_price < 900000 AND address_line_1 = '70 Karl route ';
 
-/* MODERATE: Select Houses with 2 rooms */
+/* MODERATE: Select Houses with room count */
 SELECT * FROM House INNER JOIN Property P on House.HouseID = P.PropertyID WHERE (SELECT COUNT(*) FROM Room WHERE RoomID = P.PropertyID) = 2;
 
-/* MODERATE: Select Flats with >= 2 rooms AND leasehold_frequency < 5 */
-SELECT * FROM Flat INNER JOIN Property P on Flat.FlatID = P.PropertyID WHERE (SELECT COUNT(*) FROM Room WHERE RoomID = P.PropertyID) >= 2 AND leasehold_frequency < 5;
+/* MODERATE: Select Flats with room count AND leasehold_frequency */
+SELECT * FROM Flat INNER JOIN Property P on Flat.FlatID = P.PropertyID WHERE (SELECT COUNT(*) FROM Room WHERE RoomID = P.PropertyID) >= 2 AND leasehold_frequency = 'quarterly';
 
-/* ADVANCED: Select (number of flats) with more than 50 clicks since 10 December */
+/* ADVANCED: Select (number of flats) with click count since date */
+SELECT COUNT(*) FROM Flat INNER JOIN Property P on Flat.FlatID = P.PropertyID WHERE (SELECT COUNT(*) FROM Click WHERE property_id = P.PropertyID AND dtime > '10 December 2019') > 8;
 
-/* ADVANCED: Select users who have clicked on properties more than 5 times grouped by post code */
+/* ADVANCED: Select users whose average property click is more than X times grouped by post code */
+SELECT f_name, l_name, age, post_code FROM User INNER JOIN Address A on User.address_id = A.AddressID;
