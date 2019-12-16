863f75b452858b7e36e02b68b0a8ef765d3d279a
Daniel Samet <danielsamet98@gmail.com>
2019-12-16 01:13:36 +0000

    updated queries.sql;
    Signed-off-by: Daniel Samet <danielsamet98@gmail.com>

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /* BASIC: Select Properties with sale_price AND size_unit */
 SELECT * FROM Property WHERE sale_price < 500000 AND size_unit = 'metres';
 
-/* BASIC: Select Properties with sale_price AND address*/
+/* BASIC: Select Properties with sale_price AND address */
 SELECT * FROM Property INNER JOIN Address A2 on Property.address_id = A2.AddressID WHERE sale_price < 900000 AND address_line_1 LIKE '%6%';
 
 /* MODERATE: Select Houses with room count */
@@ -11,7 +11,7 @@ SELECT * FROM House INNER JOIN Property P on House.HouseID = P.PropertyID WHERE 
 SELECT * FROM Flat INNER JOIN Property P on Flat.FlatID = P.PropertyID WHERE (SELECT COUNT(*) FROM Room WHERE property_id = P.PropertyID) >= 10 AND leasehold_frequency = 'Quarterly';
 
 /* ADVANCED: Select (number of flats) with click count since date */
-SELECT COUNT(*) FROM Flat INNER JOIN Property P on Flat.FlatID = P.PropertyID WHERE (SELECT COUNT(*) FROM Click WHERE property_id = P.PropertyID AND dtime > '2019-12-25') > 8;
+SELECT COUNT(*) FROM Flat INNER JOIN Property P on Flat.FlatID = P.PropertyID WHERE (SELECT COUNT(*) FROM Click WHERE property_id = P.PropertyID AND dtime BETWEEN '2019-10-01' AND CURDATE()) >= 3;
 
 /* ADVANCED: Select users whose average property click is more than X times grouped by post code */
-SELECT UPPER(f_name) AS FirstName, Upper(l_name) AS LastName, age AS Age, A.post_code AS PostCode, COUNT(ClickID) AS PropertyClicks FROM User INNER JOIN Address A on User.address_id = A.AddressID INNER JOIN Click C on User.UserID = C.UserID HAVING COUNT(ClickID) > AVG(ClickID) ORDER BY age DESC;
+SELECT UPPER(f_name) AS FirstName, Upper(l_name) AS LastName, age AS Age, A.post_code AS PostCode, COUNT(ClickID) AS PropertyClicks FROM User INNER JOIN Address A on User.address_id = A.AddressID INNER JOIN Click C on User.UserID = C.UserID GROUP BY Age, f_name ORDER BY f_name DESC;
