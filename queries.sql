66cd7681eb4488923a9e8f4e4f5ef8a2b7cff5dc
Daniel Samet <danielsamet98@gmail.com>
2019-12-16 00:56:59 +0000

    updated queries.sql;
    Signed-off-by: Daniel Samet <danielsamet98@gmail.com>

--- queries.sql
+++ queries.sql
@@ -11,7 +11,7 @@ SELECT * FROM House INNER JOIN Property P on House.HouseID = P.PropertyID WHERE 
 SELECT * FROM Flat INNER JOIN Property P on Flat.FlatID = P.PropertyID WHERE (SELECT COUNT(*) FROM Room WHERE property_id = P.PropertyID) >= 10 AND leasehold_frequency = 'Quarterly';
 
 /* ADVANCED: Select (number of flats) with click count since date */
-SELECT COUNT(*) FROM Flat INNER JOIN Property P on Flat.FlatID = P.PropertyID WHERE (SELECT COUNT(*) FROM Click WHERE property_id = P.PropertyID AND dtime > '10 April 2019') > 8;
+SELECT COUNT(*) FROM Flat INNER JOIN Property P on Flat.FlatID = P.PropertyID WHERE (SELECT COUNT(*) FROM Click WHERE property_id = P.PropertyID AND dtime > '2019-12-25') > 8;
 
 /* ADVANCED: Select users whose average property click is more than X times grouped by post code */
 SELECT UPPER(f_name) AS FirstName, Upper(l_name) AS LastName, age AS Age, A.post_code AS PostCode, COUNT(ClickID) AS PropertyClicks FROM User INNER JOIN Address A on User.address_id = A.AddressID INNER JOIN Click C on User.UserID = C.UserID HAVING COUNT(ClickID) > AVG(ClickID) ORDER BY age DESC;
